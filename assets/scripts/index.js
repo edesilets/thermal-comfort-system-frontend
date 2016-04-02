c6278328c926dc17778a66a5e98a91a5b0fabd14
Ethan <erdesi90562@gmail.com>
2016-04-02 18:52:55 -0400

    remove require for missing file

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,3 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
-
-// use require without a reference to ensure a file is bundled
-require('./example');
+module.exports = true;
