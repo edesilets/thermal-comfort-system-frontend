d72b587ddd24474c3b52c1e653fe0f45d0268e01
Ethan <erdesi90562@gmail.com>
2016-04-03 17:58:49 -0400

    Extract the dash from the main index file.
    If the handle bars template is not rendered the page will have the nav bar but not body.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,3 +1,16 @@
 'use strict';
 
+// handlebars template require below
+let dashTemplate = require('./handlebars/dashboard.handlebars');
+
+let uiRules = require('./ui/rules.js')
+
+
+let init = function() {
+  $('#page-wrapper').append(dashTemplate());
+  uiRules.start();
+};
+
+$(document).ready(init);
+
 module.exports = true;
