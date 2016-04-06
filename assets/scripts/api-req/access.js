e1abe225ce1c14a64b10d0263ab1a688a7ebee25
Ethan <erdesi90562@gmail.com>
2016-04-06 17:04:00 -0400

    Alter all sign** AJAX requests to use promises

--- assets/scripts/api-req/access.js
+++ assets/scripts/api-req/access.js
@@ -5,70 +5,91 @@
 const env = require('../env.js');
 
 let signUp = function(item, onSuccess, onFailure) {
-  $.ajax({
-    url: env.url + 'sign-up',
-    type: 'POST',
-    contentType: false,                     // Needed for FormData
-    processData: false,                     // Needed for FormData This is because item
-    data: item                              // item is referancing the new object called 'item'.
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: env.url + 'sign-up',
+      type: 'POST',
+      contentType: false,                     // Needed for FormData
+      processData: false,                     // Needed for FormData This is because item
+      data: item                              // item is referancing the new object called 'item'.
+    })
+    .done((result) => resolve(result))
+    .fail((reason) => reject(reason));
   })
-  .done(function (data) {
+  .then(function (data) {
     localStorage.setItem('email', data.user.email);
     localStorage.setItem('id', data.user.id);
     localStorage.setItem('token', data.user.token);
-  }, onSuccess)
-  .fail(onFailure);
+  })
+  .then(onSuccess)
+  .catch(onFailure);
 };
 
 let signIn = function(item, onSuccess, onFailure) {
-  $.ajax({
-    url: env.url + 'sign-in',
-    type: 'POST',
-    contentType: false,                     // Needed for FormData
-    processData: false,                     // Needed for FormData This is because item
-    data: item                              // item is referancing the new object called 'item'.
+  return new Promise(function (resolve, reject) {
+    $.ajax({
+      url: env.url + 'sign-in',
+      type: 'POST',
+      contentType: false,                     // Needed for FormData
+      processData: false,                     // Needed for FormData This is because item
+      data: item,                             // item is referancing the new object called 'item'.
+    })
+    .done((result) => resolve(result))
+    .fail((reason) => reject(reason));
   })
-  .done(function (data) {
+  .then(function (data) {
     localStorage.setItem('email', data.user.email);
     localStorage.setItem('id', data.user.id);
     localStorage.setItem('token', data.user.token);
-  }, onSuccess)
-  .fail(onFailure);
+  })
+  .then(onSuccess)
+  .catch(onFailure);
 };
 
 let signOut = function(onSuccess, onFailure) {
-  $.ajax({
-    url: env.url + 'sign-out/' + localStorage.getItem('id'),
-    type: 'DELETE',
-    headers: {
-        Authorization: 'Token token=' + localStorage.getItem('token'),
-      }
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: env.url + 'sign-out/' + localStorage.getItem('id'),
+      type: 'DELETE',
+      headers: {
+          Authorization: 'Token token=' + localStorage.getItem('token'),
+        }
+    })
+    .done((result) => resolve(result))   // HACK: This should be resolve but backends is sending wrong Status Code foward.
+    .fail((reason) => reject(reason));
   })
-  .done(function () {
+  .then(function (data) {
      localStorage.clear();
      console.log('Success is good!');
-  }, onSuccess)
-  .fail(function () {
-     localStorage.clear();
-     console.log('Failure is good backend needs to be fixed');
-  }, onFailure);
+  })
+  .then(onSuccess)
+  .catch(function () {
+    //localStorage.clear();
+    console.log('failure is good!');
+    // /onFailure;
+  });
 };
 
 let changePass = function(item, onSuccess, onFailure) {
-  $.ajax({
-    url: env.url + 'change-password/' + localStorage.getItem('id'),
-    type: 'PATCH',
-    headers: {
+  return new Promise(function(resolve, reject) {
+    $.ajax({
+      url: env.url + 'change-password/' + localStorage.getItem('id'),
+      type: 'PATCH',
+      headers: {
         Authorization: 'Token token=' + localStorage.getItem('token'),
       },
-    contentType: false,                     // Needed for FormData
-    processData: false,                     // Needed for FormData This is because item
-    data: item                              // item is referancing the new object called 'item'.
+      contentType: false,                     // Needed for FormData
+      processData: false,                     // Needed for FormData This is because item
+      data: item                              // item is referancing the new object called 'item'.
+    })
+    .done((result) => resolve(result))
+    .fail((reason) => reject(reason));
   })
-  .done(function (data) {
+  .then(function (data) {
     localStorage.setItem('token', data.user.token);
-  }, onSuccess)
-  .fail(onFailure);
+  })
+  .then(onSuccess)
+  .catch(onFailure);
 };
 
 module.exports = {
