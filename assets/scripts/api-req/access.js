b895214ddb817ad02f428d2c7cba5f3df1166c6b
Ethan <erdesi90562@gmail.com>
2016-04-06 14:41:01 -0400

    Alter Ajax to use localstorage

--- assets/scripts/api-req/access.js
+++ assets/scripts/api-req/access.js
@@ -12,7 +12,11 @@ let signUp = function(item, onSuccess, onFailure) {
     processData: false,                     // Needed for FormData This is because item
     data: item                              // item is referancing the new object called 'item'.
   })
-  .done(onSuccess)
+  .done(function (data) {
+    localStorage.setItem('email', data.user.email);
+    localStorage.setItem('id', data.user.id);
+    localStorage.setItem('token', data.user.token);
+  }, onSuccess)
   .fail(onFailure);
 };
 
@@ -24,34 +28,46 @@ let signIn = function(item, onSuccess, onFailure) {
     processData: false,                     // Needed for FormData This is because item
     data: item                              // item is referancing the new object called 'item'.
   })
-  .done(onSuccess)
+  .done(function (data) {
+    localStorage.setItem('email', data.user.email);
+    localStorage.setItem('id', data.user.id);
+    localStorage.setItem('token', data.user.token);
+  }, onSuccess)
   .fail(onFailure);
 };
 
 let signOut = function(onSuccess, onFailure) {
   $.ajax({
-    url: env.url + 'sign-out/' + env.user.id,
+    url: env.url + 'sign-out/' + localStorage.getItem('id'),
     type: 'DELETE',
     headers: {
-        Authorization: 'Token token=' + env.user.token,
+        Authorization: 'Token token=' + localStorage.getItem('token'),
       }
   })
-  .done(onSuccess)
-  .fail(onFailure);
+  .done(function () {
+     localStorage.clear();
+     console.log('Success is good!');
+  }, onSuccess)
+  .fail(function () {
+     localStorage.clear();
+     console.log('Failure is good backend needs to be fixed');
+  }, onFailure);
 };
 
 let changePass = function(item, onSuccess, onFailure) {
   $.ajax({
-    url: env.url + 'change-password/' + env.user.id,
+    url: env.url + 'change-password/' + localStorage.getItem('id'),
     type: 'PATCH',
     headers: {
-        Authorization: 'Token token=' + env.user.token,
+        Authorization: 'Token token=' + localStorage.getItem('token'),
       },
     contentType: false,                     // Needed for FormData
     processData: false,                     // Needed for FormData This is because item
     data: item                              // item is referancing the new object called 'item'.
   })
-  .done(onSuccess)
+  .done(function (data) {
+    localStorage.setItem('token', data.user.token);
+  }, onSuccess)
   .fail(onFailure);
 };
 
