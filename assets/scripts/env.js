172a0799fbc6583efb0c66dc73b017295476b76f
Ethan <erdesi90562@gmail.com>
2016-04-07 23:48:14 -0400

    BETA Release Rules

--- assets/scripts/env.js
+++ assets/scripts/env.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const env = {
-  urld: 'http://localhost:3000/',
+  urlLH: 'http://localhost:3000/',
   url: 'http://homestatus.ddns.net:3000/',
   ws: 'ws://homestatus.ddns.net:9001/',
 };
