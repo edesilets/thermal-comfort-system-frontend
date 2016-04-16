e0c5b0861d7b5c15582c35eb184beaf9b8edf7c9
Ethan <erdesi90562@gmail.com>
2016-04-16 08:51:06 -0400

    Stay logged in on connection loss.
    Remove unnecessary sign-out template. (Unused functionality moved to a drop down feature)
    Relocate code that was located in signActions/signedin.js to sign.js for better code readability.
    Remove pre-populated form fields in sign-Up, change password.
    Remove "Remember Me" check box from sign-In, sign-Up.
    Remove Side Nav bar links when logged out.
    Assign better naming conventions to functions in ui/sign.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,19 +2,16 @@
 
 // handlebars template require below
 // Views
-let whenLogedoutNavTemplate = require('./handlebars/sign/signinNavOut.handlebars');
 let dashTemplate = require('./handlebars/dashboard.handlebars');
 
 let uiSign = require('./ui/sign.js');
 
 let init = function() {
-  // Main Functionaliy
-  //ADD IF STATEMENT FOR localStorage CHECK IF USER IS LOGGED IN.
-  $('.userDropDown').append(whenLogedoutNavTemplate());
-  $('#page-wrapper').append(dashTemplate());
-  $('li[data-role="login"]').on('click', uiSign.In);
-  $('li[data-role="signup"]').on('click', uiSign.up);
-  // Other functions below
+  if ((localStorage.getItem('token') && localStorage.getItem('id'))) {
+    uiSign.signedInView();
+  } else {
+    uiSign.signClickHandlers();
+  }
 };
 
 $(document).ready(init);
