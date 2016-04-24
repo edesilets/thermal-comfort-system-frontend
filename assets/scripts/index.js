52112502dc652975dad321987067cd1ad97cfb0f
Ethan <erdesi90562@gmail.com>
2016-04-23 22:21:45 -0400

    Create live MQTT feeds for Dash Board View
    Create useful class names in the dashboard template. Fix alignment issues with panels.
    Add MQTT as a dependence. Show Live feeds from logged out state and logged in state.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,12 @@
 'use strict';
 
-// handlebars template require below
-// Views
-let dashTemplate = require('./handlebars/dashboard.handlebars');
-
 let uiSign = require('./ui/sign.js');
 
 let init = function() {
   if ((localStorage.getItem('token') && localStorage.getItem('id'))) {
     uiSign.signedInView();
   } else {
+    // Signed Out view includes Dash with feeds along with click hanglers for nav
     uiSign.signClickHandlers();
   }
 };
